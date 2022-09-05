884218dc50b15168bdbb761bc147470403aa13e3
Mayuca-F <myc.2623@gmail.com>
2022-09-05 14:31:13 +0900

    [fix]Webviewの実装

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,32 +13,24 @@ class MainActivity : AppCompatActivity() {
         val sub_btn = findViewById<Button>(R.id.nextButton)    //画面のボタン
 //        val navView: NavigationView = findViewById(R.id.navView)
 //        val navController = this.findNavController(R.id.proFragment)
-
 //        val navController = this.findNavController(R.id.navHostFragment)
-
-
 //        NavigationUI.setupWithNavController(navView, navController)
 
         sub_btn.setOnClickListener {
-
-
-
             //Intentオブジェクト生成、遷移画面定義
             val nextIntent = Intent(this, SubActivity::class.java)
-            //Intentオブジェクトにテキストの内容をプットする
+//            Intentオブジェクトにテキストの内容をプットする
 //            nextIntent.putExtra("main_tv", main_tv.text.toString())
+//            次のActivity実行
+            startActivity(nextIntent)
+        }
+
+        val btn = findViewById<Button>(R.id.btn_web)    //画面のボタン
+        btn.setOnClickListener {
+            //Intentオブジェクト生成、遷移画面定義
+            val nextIntent = Intent(this, WebviewActivity::class.java)
             //次のActivity実行
             startActivity(nextIntent)
-
-            val btn = findViewById<Button>(R.id.btn_web)    //画面のボタン
-
-            btn.setOnClickListener {
-                //Intentオブジェクト生成、遷移画面定義
-                val nextIntent = Intent(this, SubActivity::class.java)
-                //次のActivity実行
-                startActivity(nextIntent)
-            }
-
         }
     }
 }
